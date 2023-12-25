3a91115411b846427f78c11e3357ed66e2858b1b
sjlarrain <sjlarrain@uc.cl>
2023-12-25 19:09:38 -0300

    Exchange add :bank:

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ def main():
     app.add_handler(CommandHandler("price", price_command))
     app.add_handler(MessageHandler(filters.TEXT, authentification))
     # app.add_error_handler(error_handler)
-    
-    # app.add_handler(MessageHandler(filters.TEXT, handle_decision))
-
-    # app.add_error_handler(error)
 
     print("polling")
     app.run_polling(allowed_updates=Update.ALL_TYPES)
---
+++ trader.py
@@ -0,0 +1,25 @@
+from exchanges import Exchange
+
+
+
+class ExchangeManager:
+    def __init__(self, market):
+        self.market = market
+        self.exchange = Exchange(market)
+        self.purchases = []
+    
+    def get_balances(self):
+        self.exchage.get_balances()
+    
+    def get_price(self):
+        self.get_price("asks")
+    
+    # def buy_cripto(self):
+    #     self.exchange.buy_crypto(10000)
+    
+    # def sell_crypto(self):
+    #     self.exchange.sell_crypto(10000)
+
+        
+
+    
